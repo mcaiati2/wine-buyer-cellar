2d4463f509aa57d46391bed4e624c9e232d33779
mcaiati2 <93667565+mcaiati2@users.noreply.github.com>
2024-12-16 11:59:50 -0500

    updated navlink from wineform and shopform

--- client/src/pages/ShopForm.tsx
+++ client/src/pages/ShopForm.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
-import { NavLink, useLocation, useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const initialFormData = {
   name: '',
--- client/src/pages/WineForm.tsx
+++ client/src/pages/WineForm.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { useState } from 'react';
-import { useNavigate, useLocation, NavLink } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 const initialFormData = {
   brand: '',
