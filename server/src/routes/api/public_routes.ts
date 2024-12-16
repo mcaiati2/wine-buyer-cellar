4fe927276969bfdebe5b079e73b196112e58d1f5
mcaiati2 <93667565+mcaiati2@users.noreply.github.com>
2024-12-16 17:03:43 -0500

    router adjust

--- server/src/routes/api/public_routes.ts
+++ server/src/routes/api/public_routes.ts
@@ -1,36 +1,13 @@
-import { Router, Response } from 'express';
-import { Shop, User, Wine } from '../../models/index.js';
+import {Router} from 'express';
 
 const router = Router();
 
-// Get all shops
-router.get('/shops', async (_, res: Response) => {
-  try {
-    // Find all shops and attach the user that created the shop and also all the wines associated with the shop
-    // We use the attributes property to select the columns/fields that we want
-    // await Shop.findAll()
-    const shops = await Shop.findAll({
-      // This property is specific to associations (ie. hasMany, belongsTo, belongsToMany, hasOne)
-      include: [
-        {
-          model: Wine,
-          // This allows us to choose the fields we would like to get back on the wine objects
-          attributes: ['brand', 'type']
-        },
-        {
-          model: User,
-          // This is pulling a nested user object, so you have to make sure you DO NOT include the password field
-          // In other words, pick the fields you want
-          attributes: ['first_name', 'last_name']
-        }
-      ]
-    });
+// Middleware to parse JSON bodies/
 
-    res.json({ shops });
-  } catch (error) {
-    res.status(500).json({ error: 'An error occurred while counting shops' });
-  }
-});
+// 
+// POST endpoint to add a new recipe
+
+
 
 
 export default router;
